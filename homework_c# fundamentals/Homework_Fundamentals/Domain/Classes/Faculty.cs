5e3e767bae77a4b10176cde968ce887ea2a36161
Elena-r-code <75211054+Elena-r-code@users.noreply.github.com>
2021-12-17 09:21:02 +0100

    Add files via upload

---
+++ homework_c# fundamentals/Homework_Fundamentals/Domain/Classes/Faculty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Classes
+{
+    public class Faculty
+    {
+        public string Name { get; set; }
+        public int MaxNumberOfStudents { get; set; }
+        //public List<Professor> Professors { get; set; }
+        public List<Student> Students { get; set; }
+
+        public Faculty(string name, int maxnumberofstudents, List<Student> students)
+        {
+            Name = name;
+            MaxNumberOfStudents = maxnumberofstudents;
+            Students = students;
+        }
+        
+    }
+}
---
+++ homework_c# fundamentals/Homework_Fundamentals/Domain/Classes/Student.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Classes
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool Driving { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfEnrollment { get; set; }
+        public long EMBG { get; set; }
+        public TypeOfStudent TypeOfStudent { get; set; }
+        public string MartialStatus { get; set; }
+        public DateTime DateGraduated { get; set; }
+        
+
+        public Student(string name, string surname, bool driving, string email, DateTime dateofenrollment, long embg, TypeOfStudent typeOfStudent, string martialstatus, DateTime dategraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Driving = driving;
+            Email = email;
+            DateOfEnrollment = dateofenrollment;
+            EMBG = embg;
+            TypeOfStudent = typeOfStudent;
+            MartialStatus = martialstatus;
+            DateGraduated = dategraduated;
+            
+        }
+    }
+}
