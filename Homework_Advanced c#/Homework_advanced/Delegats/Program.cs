32cabba99426429de4542f215536c7b245a654f9
Elena-r-code <75211054+Elena-r-code@users.noreply.github.com>
2021-12-18 02:12:59 +0100

    Add files via upload

---
+++ Homework_Advanced c#/Homework_advanced/Delegats/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Delegats
+{
+    class Program
+    {
+        public static void Menu()
+        {
+            Console.WriteLine("Choose option: 1, 2 or 3 ");
+            Console.WriteLine("1. Print the name.");
+            Console.WriteLine("2. Print the name and info. ");
+            Console.WriteLine("3. Print name, date and time. ");
+            string input = Console.ReadLine();
+            bool success = int.TryParse(input, out int parsed);
+            if (success && (parsed == 1 || parsed ==2 ||parsed ==3))
+            {
+                Console.WriteLine("Enter name, surname and index number of the student :");
+                string input2 = Console.ReadLine();
+
+                string[] parts = input2.Split(" ");
+                string name = parts[0];
+                string surname = parts[1];
+                string index = parts[2].ToString();
+
+             if(parsed == 1) 
+                {
+                    string option1 = name + " \n Studentot se logirase na sistemot " ;
+                    Action<string> PrintFirst = delegate (string str)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(str);
+                        Console.ResetColor();
+                    };
+                    PrintFirst(option1);
+                }
+             if(parsed ==2)
+                {
+                    Del del1 = PrintName;
+                    Del del2 = PrintLog;
+                    Del del3 = PrintSurname;
+                    Del del4 = PrintIndex;
+
+                    del1(name);
+                    del2("Studentot se logirase na sistemot!");
+                    del1(name);
+                    del3(surname);
+                    del4(index);
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please choose one of the following options:  1, 2 or 3");
+                Console.ResetColor();
+            }
+        }
+        delegate void Del(string str);
+
+        static void PrintName(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+        static void PrintLog(string str)
+        {
+            Console.WriteLine(str);
+        }
+        static void PrintSurname(string surname)
+        {
+            Console.WriteLine(surname);
+        }
+        static void PrintIndex(string index)
+        {
+            Console.WriteLine(index);
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Menu();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework_Advanced c#/Homework_advanced/Homework_advanced/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework_advanced
+{
+    class Program
+    {
+        public static int squareDigits(int n)
+        {
+            string numbers = "";
+            foreach(char s in n.ToString())
+            {
+                int i = Int32.Parse(s.ToString());
+                double result = Math.Pow(i, 2);
+                numbers += Convert.ToString(result);
+            }
+            return Convert.ToInt32(numbers);
+  
+        }
+
+        public static int DescendingOrder(int num)
+        {
+            int n = Math.Abs(num);
+            int l = ((int)Math.Log10(n > 0 ? n : 1)) + 1;
+            int[] array = new int[l];
+            for(int i =0; i < l; i++)
+            {
+                array[(l - i) - 1] = n % 10;
+                n /= 10;
+
+            }
+            if(num < 0)
+            {
+                array[0] *= 1;                      
+            }
+            Array.Sort(array);
+            Array.Reverse(array);
+            int h = 0;
+            for(int i =0; i<array.Length; i++)
+            {
+                int index = array.Length - i - 1;
+                h += ((int)Math.Pow(10, index)) * array[i];
+            }
+            return h;
+            
+
+        }
+        public static bool IsPangram(string str)
+        {
+            return str.ToLower().Where(ch => char.IsLetter(ch)).GroupBy(ch => ch).Count() ==26;
+        }
+
+        public static int sumTwoSmallestNumbers(int[] numbers)
+        {
+            return numbers.OrderBy(x => x).Take(2).Sum();
+        }
+
+        public static int GetVowelCount(string str)
+        {
+            int result =0;
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            result = str.Count(x => vowels.Contains(x));
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(squareDigits(9119));
+
+            Console.WriteLine( DescendingOrder(42145));
+            Console.WriteLine( DescendingOrder(145263));
+            Console.WriteLine(DescendingOrder(123456789));
+         
+            string str= ("The quick brown fox jumps over the lazy dog");
+            Console.WriteLine("{0} :\"{1}\"", IsPangram(str), str);
+
+            int[] array = { 19, 5, 42, 2, 77 };
+            int[] array2 = { 10,343445353, 3453445, 345354533 };
+            Console.WriteLine(sumTwoSmallestNumbers(array));
+            Console.WriteLine(sumTwoSmallestNumbers(array2));
+
+            Console.WriteLine("Enter sentance to count the vowels:");
+            string sentance = Console.ReadLine();
+            Console.WriteLine("Number of vowels : ");
+            Console.Write(GetVowelCount(sentance));
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
