3ed85d2a3cba5b09a1c1b52065932d2ac7fc8838
Elena-r-code <75211054+Elena-r-code@users.noreply.github.com>
2021-12-20 08:38:30 +0100

    Add files via upload

---
+++ AsyncAwaitHomework/Homework/Homework/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        public static async Task<List<int>> ReadDataAsync(string path)
+        {
+            List<int> numbers = new List<int>();
+            string line;
+
+            try
+            {
+
+                StreamReader sr = new StreamReader(path);
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    numbers.Add(int.Parse(line));
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception " + e.Message);
+            }
+            return numbers;
+        }
+
+        public static async Task<int> SumOfNumbersAync(List<int> numbers)
+        {
+            string appPath = @"..\..\..\";
+            string folderPath = appPath + "TextFile";
+            string filePath = folderPath + @"\DummyNumbers.txt";
+
+            numbers = await Task.Run((string filePath)
+                =>
+            {
+                ReadDataAsync(filePath);
+
+            });
+            int sum = 0;
+            foreach(int n in numbers)
+            {
+                sum += n;
+            }
+            return sum;
+
+        }
+        static void Main(string[] args)
+        {
+            string appPath = @"..\..\..\";
+            string folderPath = appPath + "TextFile";
+            string filePath = folderPath + @"\DummyNumbers.txt";
+            
+            ReadDataAsync(filePath);
+
+            SumOfNumbersAync(ReadDataAsync(filePath));
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
